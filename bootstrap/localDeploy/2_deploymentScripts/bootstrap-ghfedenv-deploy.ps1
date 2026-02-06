33df0ac60e62505afc83c683453ce099a2b4eefb
JamieP <118207133+JamieP0101@users.noreply.github.com>
2026-02-06 10:15:05 +0000

    Update bootstrap-ghfedenv-deploy.ps1

--- bootstrap/localDeploy/2_deploymentScripts/bootstrap-ghfedenv-deploy.ps1
+++ bootstrap/localDeploy/2_deploymentScripts/bootstrap-ghfedenv-deploy.ps1
@@ -1,5 +1,5 @@
 
-$TenantID = "6771b25a-f4d8-4f9f-9fcc-e7468a5cdc46"
+$TenantID = "<Fill Me In>"
 
 $PAT = Read-Host -AsSecureString "Enter GitHub Personal Access Token (ghPAT)"
 $PATPlain = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($PAT))

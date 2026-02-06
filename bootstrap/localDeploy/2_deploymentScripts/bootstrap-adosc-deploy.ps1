5f7ba77b8d70539b5379c235ec174287bf6627e9
JamieP <118207133+JamieP0101@users.noreply.github.com>
2026-02-06 10:14:46 +0000

    Update bootstrap-adosc-deploy.ps1

--- bootstrap/localDeploy/2_deploymentScripts/bootstrap-adosc-deploy.ps1
+++ bootstrap/localDeploy/2_deploymentScripts/bootstrap-adosc-deploy.ps1
@@ -1,5 +1,5 @@
 
-$TenantID = "6771b25a-f4d8-4f9f-9fcc-e7468a5cdc46"
+$TenantID = "<Fill Me In>"
 
 $PAT = Read-Host -AsSecureString "Enter Azure DevOps Personal Access Token (adoPAT)"
 $PATPlain = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($PAT))
